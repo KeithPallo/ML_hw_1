32f46f8209152478d3c3bfd91f0f9d7d5e36e2fa
KeithPallo <Keith.n.pallo@gmail.com>
2018-10-16 21:29:10 -0500

    late_commit

--- ID3.py
+++ ID3.py
@@ -3,6 +3,7 @@ import math
 
 
 def ID3(examples, default, root = True):
+  #print("TEST")
   '''
   Takes in an array of examples, and returns a tree (an instance of Node)
   trained on the examples.  Each example is a dictionary of attribute:value pairs,
@@ -24,17 +25,19 @@ def ID3(examples, default, root = True):
 
 
   if not examples_c:
-    return Node(label = default)
+    return Node(label = default, test_mode = choose_mode(examples_c))
 
   elif same_class(examples_c) or no_non_trivial(examples_c):
     mode = choose_mode(examples_c)
-    return Node(label = mode)
+    return Node(label = mode,  test_mode = mode)
 
   else:
       #print(examples)
       attribute, examples_list = choose_best(examples_c)
-      t = Node(label = attribute)
+      #print(attribute)
       mode = choose_mode(examples_c)
+      #print(mode)
+      t = Node(label = attribute, test_mode =  mode)
 
       for i in examples_list:
         split_attribute = i[0][attribute]
@@ -50,18 +53,63 @@ def ID3(examples, default, root = True):
 
 
 
-def prune(node, examples):
+def prune_2(node, examples):
   '''
   Takes in a trained tree and a validation set of examples.  Prunes nodes in order
   to improve accuracy on the validation data; the precise pruning strategy is up to you.
   '''
-  # check to see type of node
-  # if leaf, test vs. mode of current
+  #print(examples)
+  #print(node.label)
+  if node.is_leaf() == True:
+      return
 
-  # split and pass based on attributes
 
-  # pass in examples to current node
-  pass
+  # if any children are not leaves, create examples for them and call prune
+
+  for i in node.children.items():
+      if i[1].is_leaf() == False:
+          pass_examples = [ x for x in examples if x[node.label] == i[0]]
+          prune(i[1],pass_examples)
+
+  prune_command = prune_bool(node,examples)
+
+  node.self_prune(prune_command)
+
+
+def prune_bool(node,examples):
+
+  acc_prune = prune_test(node,examples)
+  acc_train = test(node,examples)
+  #print(acc_prune,acc_train)
+
+  if acc_prune > acc_train:
+    #print("test",node.label)
+    return True
+  else:
+    return False
+
+
+def prune_test(node,examples):
+  #print(node.label,examples)
+  #print(node.test_mode)
+  num_tested = 0
+  num_correct = 0
+
+  for i in examples:
+      #print(i)
+    num_tested += 1
+
+    predict = node.prune_evaluate(i)
+    correct = i["Class"]
+    #print(predict,correct)
+
+    if predict == correct:
+      num_correct += 1
+
+  if num_tested == 0:
+      return 0
+  #print(num_correct)
+  return (num_correct / num_tested)
 
 def test(node, examples):
   '''
@@ -82,18 +130,10 @@ def test(node, examples):
     if predict == correct:
       num_correct += 1
 
+  if num_tested == 0:
+    return 0
+
   return (num_correct / num_tested)
-
-  # num correct
-  # num tested
-
-  # Predict value
-  # correct or not
-  # update ratio
-
-  # return accuracy
-
-  pass
 
 
 def evaluate(node, example):
@@ -109,11 +149,6 @@ def evaluate(node, example):
 
 # Custom definitions created
 # -----------------------------------------------------------------------------
-def test_example(node,example):
-    pass
-
-def ID3_build(examples,default):
-    pass
 
 def choose_best(examples):
 
@@ -261,13 +296,102 @@ def no_non_trivial(examples):
 """
 
 
+def prune(node, examples):
+  '''
+  Takes in a trained tree and a validation set of examples.  Prunes nodes in order
+  to improve accuracy on the validation data; the precise pruning strategy is up to you.
+  '''
+
+  potential = []
+  x = True
+
+  while x == True:
+    # other case  - determine if all  children. If so test on self
+    if node.all_children_leaf() == True:
+      potential = [node]
+      #print("test")
+
+      bool = prune_if_possible(potential,node,examples,current_accuracy)
+      #print(bool)
+      if bool == False:
+        x = False
+
+
+    else:
+      potential = find_potential_prunes(node)
+      #print(potential)
+     # get current accuracy on the validation set using standard test
+      current_accuracy = test(node,examples)
+
+      # Pass to prune if possible
+      bool = prune_if_possible(potential,node,examples,current_accuracy)
+      #print(bool)
+      if bool == False:
+        x = False
+
+
+def prune_if_possible(list_of_node,root,examples,current_accuracy):
+
+  max_accuracy = 0
+  pointer = None
+
+  for i in list_of_node:
+    holder = i.label
+    i.label = i.test_mode
+    i.type = "Prune"
+    prune_accuracy = test(root,examples)
+    #print(prune_accuracy)
+    if prune_accuracy > max_accuracy:
+      max_accuracy = prune_accuracy
+      pointer = i
+
+    i.label = holder
+    i.type = "Split"
+
+  if max_accuracy > current_accuracy:
+    print("MUTATE")
+    pointer.self_prune(True)
+    return True
+
+  return False
+
+
+
+
+  # for each node in the prune_evaluate:
+      # directly mutate label value to mode values
+      # test accuracy
+      # update initialize variables
+
+  # See if need to prune:
+      # if prune, recursively call
+      # else return
+
+def find_potential_prunes(node):
+    #itype = None
+    #rtype = List of Nodes
+    potential = []
+
+    if node.all_children_leaf() == True:
+        return
+
+    for i in node.children.values():
+        if i.type == "Split":
+            potential.append(i)
+        else:
+            potential_below = find_potential_prunes(i)
+            potential.extend(potential_below)
+
+    return potential
+
+
 
 #print(type(data))
 #print(same_class(data))
 #print(ID3(data,"fail"))
 
-data = [dict(a=1, b=0, Class=0), dict(a=1, b=1, Class=1)]
-tree = ID3(data, 0)
+#data = [dict(a=1, b=0, Class=0), dict(a=1, b=1, Class=1)]
+#tree = ID3(data, 0)
 #print(tree.children)
 #print(tree.label)
 #print(evaluate(tree, dict(a=1, b=0)))
--- node.py
+++ node.py
@@ -1,20 +1,21 @@
 class Node:
-  def __init__(self, label = False):
-    self.label = label # Class type or attribute
+  def __init__(self, label = False, test_mode = None):
+    self.label = label # Class type or attribute to split on
     self.children = {}
     self.parent_split = None
     self.type = "Leaf"
-    self.is_pruned = False
-    self.pruned_mode = None
-	# you may want to add additional fields here...
+    self.test_mode = test_mode
+
+    # you may want to add additional fields here...
     # self.examples = examples
     #self.type = "Leaf" # Leaf or Split
 
     # If examples are empty, then this is a leaf node and we use label
     # to test
+
   def add_subtree(self,subtree,attribute):
     self.children[attribute] = subtree
-    self.type = "Value"
+    self.type = "Split"
 
   def split_type(self):
     return self.label
@@ -24,21 +25,59 @@ class Node:
       return self.label
 
     else:
-      split = example[self.label]
-      sub = self.children[split]
-      return sub.evaluate(example)
+
+      try:
+        split = example[self.label]
+        sub = self.children[split]
+        return sub.evaluate(example)
+      except:
+        return self.test_mode
+
 
 
   def add_parent_split(self,parent_split):
     self.parent_split = parent_split
 
-  def cross_validate(self,examples):
-    if self.is_leaf():
-
-    for i in examples:
 
   def is_leaf(self):
-    if len(self.children) == 0 or self.type == "Leaf":
+    if len(self.children) == 0 or self.type == "Leaf" or self.type == "Prune":
       return True
     else:
       return False
+
+  def prune_evaluate(self,example):
+    if self.all_children_leaf() == True:
+      #print(self.test_mode)
+      return self.test_mode
+
+    else:
+      try:
+        split = example[self.label]
+        sub = self.children[split]
+        return sub.evaluate(example)
+      except:
+        return self.test_mode
+
+      #split = example[self.label]
+      #sub = self.children[split]
+      #return sub.prune_evaluate(example)
+
+  def all_children_leaf(self):
+    if self.is_leaf():
+      return False
+    else:
+      for i in self.children.values():
+        if i.is_leaf():
+          pass
+        else:
+          return False
+
+      return True
+
+  def self_prune(self,bool):
+      if bool == True:
+          self.children = {}
+          self.type  = "Leaf"
+          self.label = self.test_mode
+      else:
+          return
